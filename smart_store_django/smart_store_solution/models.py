6eed3540e7b6f6d2dfdee00b9bcfa446492fd959
양준 <yangjune@yangjun-ui-MacBookAir.local>
2021-04-18 23:50:22 +0900

    User(models.Model) -> User(AbstractBaseUser) for django Auth Permissions

--- smart_store_django/smart_store_solution/models.py
+++ smart_store_django/smart_store_solution/models.py
@@ -1,12 +1,36 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
 
 # Create your models here.
 
-class User(AbstractUser):
+class UserManager(BaseUserManager):
+    def create_user(self, kakao_id, nickname, password=None):
+        """
+        주어진 이메일, 닉네임, 비밀번호 등 개인정보로 User 인스턴스 생성
+        """
+        user = self.model(kakao_id=kakao_id, nickname=nickname)
+        user.set_unusable_password()
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self, kakao_id, password):
+        """
+        주어진 이메일, 닉네임, 비밀번호 등 개인정보로 User 인스턴스 생성
+        단, 최상위 사용자이므로 권한을 부여한다. 
+        """
+        user = self.model(kakao_id=kakao_id, password=password)
+        user.is_superuser = True
+        return user
+
+class User(AbstractBaseUser, PermissionsMixin):
     User_pk = models.AutoField(primary_key=True)
-    kakao_id = models.IntegerField()
+    kakao_id = models.IntegerField(unique=True)
     nickname = models.CharField(max_length=40, null=True, blank=True)
+    
+    objects = UserManager()
+    
+    USERNAME_FIELD = 'kakao_id'
+    REQUIRED_FIELDS = []
     
     def __str__(self):
         return str(self.kakao_id)
