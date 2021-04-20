90c732725c6e464c83888def051b84ea0384acff
양준 <tialem94@gmail.com>
2021-04-20 23:18:45 +0900

    github grass test and sessionauthentication test ...ing

--- smart_store_django/smart_store_project/settings.py
+++ smart_store_django/smart_store_project/settings.py
@@ -87,8 +87,8 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': [
         #'rest_framework.authentication.BasicAuthentication',
-        #'rest_framework.authentication.SessionAuthentication',
-        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        #'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
     ],
 }
 
