bc56c534f61aa7441827da67ee21c78a1f024cef
owaldeff <tialem94@gmail.com>
2021-05-16 19:25:14 +0900

    testing... change ALLOWED HOSTS

--- smart_store_django/smart_store_project/settings.py
+++ smart_store_django/smart_store_project/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.compute.amazonaws.com', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['.compute.amazonaws.com', '127.0.0.1', '127.0.0.1:3000', '127.0.0.1:8000', 'localhost', 'localhost:3000', 'localhost:8000']
 
 
 # Application definition
