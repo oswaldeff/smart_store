9d0ab74d47714f26d152c514b5ba0021d694c956
owaldeff <tialem94@gmail.com>
2021-05-25 19:39:23 +0900

    setting... reverse proxy through nginx.conf

--- smart_store_django/smart_store_solution/views.py
+++ smart_store_django/smart_store_solution/views.py
@@ -299,8 +299,7 @@ def User_delete(request):
 def cookie_set(request):
     res = JsonResponse({'message': 'COOKIE SET SUCCESS'}, status=200)
     test_cookie = 'ThisIsTestCookie'
-    #res.set_cookie('test_cookie', value=test_cookie, max_age=1000, expires=True, path='/', domain=None, httponly=False, secure=False, samesite=None)
-    res.set_cookie('test_cookie', value=test_cookie, max_age=1000, expires=True, path='/', secure=True, samesite='None')#httponly=True, 
+    res.set_cookie('test_cookie', value=test_cookie, max_age=1000, expires=True, path='/', domain=None, httponly=False, secure=False, samesite='None')
     print("set cookie complete!")
     return res
 
