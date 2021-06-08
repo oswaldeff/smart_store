c6fbad030f705acc200cfd2c8e0f824a4b882abf
owaldeff <tialem94@gmail.com>
2021-06-08 16:40:38 +0900

    CORS setting test2..

--- smart_store_django/smart_store_solution/views.py
+++ smart_store_django/smart_store_solution/views.py
@@ -300,7 +300,7 @@ def cookie_set(request):
     if request.method == 'GET':
         res = JsonResponse({'MSG': 'COOKIE SET SUCCESS'}, status=200)
         test_cookie = 'ThisIsTestCookie'
-        res.set_cookie('test_cookie', value=test_cookie, max_age=60*60*24*1, expires=True, path='/', domain=None, httponly='False', samesite='Lax')
+        res.set_cookie('test_cookie', value=test_cookie, max_age=60*60*24*1, expires=True, path='/') # , domain=None, httponly='False', samesite='Lax'
         print("set cookie complete!")
         return res
     else:
