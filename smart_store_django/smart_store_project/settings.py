d6e59e02ffeb879d7bac3d99be81fec1656dd0c7
owaldeff <tialem94@gmail.com>
2021-06-09 22:58:09 +0900

    DEBUG=True -> False

--- smart_store_django/smart_store_project/settings.py
+++ smart_store_django/smart_store_project/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.compute.amazonaws.com', '.amazonaws.com', '127.0.0.1', 'localhost']
 
