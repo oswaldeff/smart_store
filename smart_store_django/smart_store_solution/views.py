6d9b75b2a100e29fc882d33e4c533423d78cd651
owaldeff <tialem94@gmail.com>
2021-06-15 16:34:41 +0900

    use requests.get url

--- smart_store_django/smart_store_solution/views.py
+++ smart_store_django/smart_store_solution/views.py
@@ -143,6 +143,8 @@ def kakao_login(request):
         print('req header: ', request.headers)
         print('req Authorization: ', request.headers['Authorization'])
         print('req cookie: ', request.COOKIES)
+        cook = requests.get(r'http://smartstore-test90.s3-website.ap-northeast-2.amazonaws.com/').cookies.items()
+        print('cook: ', cook)
         if 'access_token' in request.session:
             access_token = request.session['access_token']
             print('from session: ', access_token)
